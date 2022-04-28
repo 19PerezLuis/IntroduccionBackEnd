80ef5a4be379091f424429634dd409f7c16a9958
Luis Fernando Perez Rodriguez <luishost131@gmail.com>
2022-04-28 11:49:34 -0600

    Se agregaron los codigos de error a la pagina

--- servidor.js
+++ servidor.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var path = require ('path');
+const { ifError } = require('assert');
 
 http.createServer(function(request,responce){
     console.log('request',request.url);
@@ -8,7 +9,6 @@ http.createServer(function(request,responce){
     if(filepath == './'){
         filepath = './index.html';
     }
-});
 var extname = string(path.extname(filepath)).toLowerCase();
 var contentType = 'text/html';
 var mimeTypes ={
@@ -17,3 +17,24 @@ var mimeTypes ={
     '.css':'text/css',
     '.png':'image/png'
 };
+fs.readFile(filepath,function(error,content){
+    if(error){
+        if(error.code == 'ENOENT'){
+            fs.readFile('/IntroduccionBackEnd/404/dist/index.html', function(error, Content){
+                responce.writeHead(200,{'Conten-Type':contentType});
+                responce.end(Content,'utf-8');
+            });
+        }
+        else{
+            responce.writeHead(500);
+            responce.end('sorry, check with the site admin for error'+error.);
+            responce.end();
+        }
+    }
+    else{
+        responce.writeHead(200,{'content-Type':contentType});
+        responce.end(content,'uft-8');
+    }
+});
+}).listen(3000);
+console.log('Server running at http://127.0.0.1:3000');
