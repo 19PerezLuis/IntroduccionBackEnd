c4c70cd86ff94484f608fc8a862068deb348d611
Luis Fernando Perez Rodriguez <luishost131@gmail.com>
2022-05-02 19:44:51 -0600

    Complemento de servidor.js para poder ejecutarlo con nodejs

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
-//llamamos el modulo http
 const http = require('http');
-//establecemos la url o IP de nuestro servidor
-const hostname = '192.168.50.189';
-//establecemos el puesto de escucha
+const hostname = '192.168.1.64';
 const port = '3000';
-//creamos una instancia HTTP con un reques y un responce
 const server = http.createServer((req,res)=>{
-    //el servidor respondera un codigo 200
     res.statusCode = 200;
-    //el servidor respondera con un texto plano
-    res.setHeader('Content-Type','text/plain');
-    //el servidor respondera el mensaje hola mundo
+    res.setHeader('Content-Type','text/html');
     res.end('<h1>Hola mundo</h1>');
 });
 server.listen(port, hostname,()=>{
--- servidor.js
+++ servidor.js
@@ -1,40 +1,39 @@
-var http = require('http');
-var fs = require('fs');
+var http = require ('http');
+var fs = require ('fs');
 var path = require ('path');
-const { ifError } = require('assert');
 
-http.createServer(function(request,responce){
-    console.log('request',request.url);
-    var filepath = '.' + request.url;
-    if(filepath == './'){
-        filepath = './index.html';
+http.createServer(function(request, response){
+    console.log('request', request.url);
+    var filePath = '.' + request.url;
+    if(filePath == './'){
+        filePath = "./index.html";
     }
-var extname = string(path.extname(filepath)).toLowerCase();
-var contentType = 'text/html';
-var mimeTypes ={
-    '.html': 'text/html',
-    '.js':'text/javascript',
-    '.css':'text/css',
-    '.png':'image/png'
-};
-fs.readFile(filepath,function(error,content){
-    if(error){
-        if(error.code == 'ENOENT'){
-            fs.readFile('/IntroduccionBackEnd/404/dist/index.html', function(error, Content){
-                responce.writeHead(200,{'Conten-Type':contentType});
-                responce.end(Content,'utf-8');
-            });
+    var extname = String(path.extname(filePath)).toLowerCase();
+    var contentType = 'text/html';
+    var mimeTypes = {
+        '.html' : 'text/html', 
+        '.js' : 'text/javascript',
+        '.css' : 'text/css',
+        '.jpg' : 'image/jpg'
+    };
+    contentType = mimeTypes[extname] || 'application/octet-stream';
+    fs.readFile(filePath, function(error, content){
+        if(error){
+            if(error.code == 'ENOENT'){
+                fs.readFile('./404.html',function(error,content){
+                   response.writeHead(200,{'Content-Type': contentType});
+                   response.end(content, 'utf-8'); 
+                });
+            }
+            else{
+                response.writeHead(500);
+                response.end('sorry, check with the site admin for error: '+ error.code+'..\n');
+                response.end();
+            }
+        }else{
+            response.writeHead(200, { 'Content-Type': contentType});
+            response.end(content,'utf-8');
         }
-        else{
-            responce.writeHead(500);
-            responce.end('sorry, check with the site admin for error'+error.);
-            responce.end();
-        }
-    }
-    else{
-        responce.writeHead(200,{'content-Type':contentType});
-        responce.end(content,'uft-8');
-    }
-});
+    });
 }).listen(3000);
-console.log('Server running at http://127.0.0.1:3000');
+console.log('Server running at http://127.0.0.1:3000/');
